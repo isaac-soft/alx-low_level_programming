6a88a958b86e239fe73db5cf67f29d0ad0b86a9f
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-04 01:38:38 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main (void)
+/**
+  * main - Entry point
+  * Description: This program prints the sentence for the task
+  * Return: 0 to end the funciton
+ */
+int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
