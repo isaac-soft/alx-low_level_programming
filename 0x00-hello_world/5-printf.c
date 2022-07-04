0c41b4605865528b3354d46f5a7f8fe30ab925b5
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-04 01:46:54 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main - Print a string
+  *Description: use printf
+  *Return: 0
+*/
+int main(void)
+{
+	prinf("%s", with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
