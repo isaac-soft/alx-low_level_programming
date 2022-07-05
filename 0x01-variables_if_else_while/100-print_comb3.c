192c0df9f5fcdf6ef03783d286a6bc9e1c2a0987
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-04 19:32:41 -0700

    Task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
   * main -main block
   * Descritpon: Print all possible cobinations of atwo digits
   * Nubers must be separated by commas and as space
@@ -8,7 +8,7 @@
   * Nubers should be printed in ascending order, with two dits.
   * YOu can only use 'putchar'.
   * You can only use 'putchar' upt to 5 times.
-  * YOu are not allowed to sue andy variable of type 'char' 
+  * YOu are not allowed to sue andy variable of type 'char'
   * Return: 0
   */
 int main(void)
@@ -19,7 +19,7 @@ int main(void)
 	while (i < 100)
 	{
 		j = i % 10; /*singles digit */
-		k = i /10; /* doubles digit */
+		k = i / 10; /* doubles digit */
 		if (k < j)
 		{
 			putchar(k + '0');
