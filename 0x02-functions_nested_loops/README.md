1ad233d074278a7f5f314baa14ccc328f043a361
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-05 22:51:48 -0700

    Stores needed functions and prototypes
    The files committed will ensure the _putchar function and test function is available at main.c

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+# ALX low level programming
+## Functions and nested loop
+
+This directory contains low level programming for solving functions and nested loops in C
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/*
+ * This file contains my function prototype
+*/
+
+int _putchar(char c);
+void printtest(void);
+
+#endif /*_MAIN_H_ */
