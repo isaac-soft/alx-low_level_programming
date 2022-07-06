e271499e3546517254b0acdc8a10f3dd26cb93eb
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 06:00:57 -0700

    Check for sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main"
+/**
+ * print_sign - checks for the sign of a number
+ * Description - This program checks if a sign is + or -
+ *
+ * @n: this parameter receives the arguments assed
+ * Return: 1 (positive) 0 (Zero) -1 (negative)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+		_putchar('-');
+		return (-1);
+}
