350f4035a7135f4737e2af30881b6986328458f1
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 12:42:47 -0700

    trying 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,36 @@
 
 int print_last_digit(int c)
 {
+	int r;
+
+
+
+	if (n < 0)
+
+	{
+
+		r = -1 * (n % 10);
+
+		_putchar(r + '0');
+
+		return (r);
+
+	}
+
+	else
+
+	{
+
+		r = n % 10;
+
+		_putchar(r + '0');
+		return (r);
+	}
+}
+
+		return (r);
+
+	}
 	c = abs(c);
 	c = c % 10;
 	_putchar(c + '0');
