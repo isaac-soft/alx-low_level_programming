18b701406705efc618b129caf3401c0a6d15dc18
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 06:15:30 -0700

    Update on absolute file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,12 @@
 
 /**
  * _abs - checks for the absolute value
+ * @int: accepts int arguments
  *
  * Return: returns the absolute value on exit
  */
 
 int _abs(int)
 {
-	return abs(int);
+	return (abs(int));
 }
