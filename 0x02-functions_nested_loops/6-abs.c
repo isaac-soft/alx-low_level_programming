e0f299ffe85e91c3458525377f368f2ae8de995a
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 06:12:08 -0700

    Compute the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - checks for the absolute value
+ *
+ * Return: returns the absolute value on exit
+ */
+
+int _abs(int)
+{
+	return abs(int);
+}
