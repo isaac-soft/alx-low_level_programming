8481be8be3542e2c82c31b77ed035fe77b9efad1
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 06:56:48 -0700

    check for last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_last_digit - prints last digit
+ * Description - Use absolute function to make checkeasy
+ * @c: accepts the arguments to be checked
+ *
+ * Return: Returns the last digit
+ */
+
+int print_last_digit(int c)
+{
+	c = abs(c);
+
+	if (c % 10 == 0)
+	{
+		c = c % 10;
+		return (c);
+	}
+	else
+	{
+		return (0);
+	}
+}
