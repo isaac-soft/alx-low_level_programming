512045c5b6c084336962c62b04c0667d958d2ccb
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 13:14:00 -0700

    Trying again

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,48 +1,28 @@
-#include <stdlib.h>
 #include "main.h"
+
 /**
- * print_last_digit - prints last digit
- * Description - Use absolute function to make checkeasy
- * @c: accepts the arguments to be checked
+ * print_last_digit - Prints the last digit
+ * of a number.
  *
- * Return: Returns the last digit
+ * @n: ininput nmber as an integer
+ *
+ * Return: last digit.
  */
 
-int print_last_digit(int c)
+int print_last_digit(int n)
 {
 	int r;
 
-
-
 	if (n < 0)
-
 	{
-
 		r = -1 * (n % 10);
-
 		_putchar(r + '0');
-
 		return (r);
-
 	}
-
 	else
-
 	{
-
 		r = n % 10;
-
 		_putchar(r + '0');
 		return (r);
 	}
-}
-
-		return (r);
-
-	}
-	c = abs(c);
-	c = c % 10;
-	_putchar(c + '0');
-
-	return (c);
 }
