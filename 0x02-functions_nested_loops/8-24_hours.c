b4ff61b9c6b09bf89b9e928f00c536164c2c1256
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 16:24:01 -0700

    Print 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,39 +1,26 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day
- * Description - it starts from 00:00 to 23:59
- *
- */
-
+  * jack_bauer - prints time
+  * Descriptin: prints every minute from
+  * 00:00 to 23:59
+  *
+  * No return value
+  */
 void jack_bauer(void)
 {
-	char i, j, k, l;
+	int i, j;
 
-	for (i = 0; i < 3; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j < 10 ; j++)
+		for (j = 0; j < 60; j++)
 		{
-			if (i != '2' && j != '3' && k != '5' && l != '9')
-			{
-				for (k = 0; k < 6; k++)
-				{
-					for (l = 0; l < 10; l++)
-					{
-						_putchar(i + '0');
-						_putchar(j + '0');
-						_putchar(':');
-						_putchar(k + '0');
-						_putchar(l + '0');
-						_putchar('\n');
-					}
-				}
-		
-			}
-			else
-			{
-				break;
-			}
+			_putchar(i / 10 + 48);
+			_putchar(i % 10 _ 48);
+			_putchar(':');
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			_putchar('\n');
 		}
 	}
 }
