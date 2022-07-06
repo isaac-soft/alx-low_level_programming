6ebcfb69f7ee3ab627c19f62775327bf97d4ff78
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 16:25:44 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 		for (j = 0; j < 60; j++)
 		{
 			_putchar(i / 10 + 48);
-			_putchar(i % 10 _ 48);
+			_putchar(i % 10 + 48);
 			_putchar(':');
 			_putchar(j / 10 + 48);
 			_putchar(j % 10 + 48);
