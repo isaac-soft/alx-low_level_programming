0d12abd269744ded8067b4bc3ad2f6f20a57dc0c
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-06 21:16:13 -0700

    Timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_times_table - PRINTS N TIMES TABLe
+ * print_times_table - PRINTS N TIMES TABL
+ * @n: receives integer arguments.
  */
 
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
-		for (i =  0; i <= n; i ++)
+		for (i =  0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
@@ -34,16 +36,16 @@ void print_times_table(int n)
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else if (k > = 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(((k / 10) % 10) + '0');
-				_putchar((k % 10) + '0');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
