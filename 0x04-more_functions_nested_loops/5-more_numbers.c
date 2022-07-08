320a66dddf819d4b41cd6ef75d23aa5ff6c1bacc
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-08 16:40:06 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 0; j < 15; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
