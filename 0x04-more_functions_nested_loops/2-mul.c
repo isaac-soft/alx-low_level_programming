7e21df309a4d3e5514a4d92b7d2511729cc6c352
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-08 13:10:30 -0700

    multiply two parameters

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - returns the multiplication of two numbers
+ *
+ * @a - 11st parameter
+ * @b -secondparameter
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
