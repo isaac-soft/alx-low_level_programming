df3625bbffd524d12adfb47257f6d3854f28f598
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-08 16:59:38 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - this codes prints n lines
+ * @n: The integer
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
