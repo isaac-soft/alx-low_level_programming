9c882f8b5c14fa40b9f085232cc4b7e86124c026
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-12 18:56:47 -0700

    Task 4
    Prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -prints string in reverse order
+ * @c: parameter
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i, length;
+	char temp;
+
+	length = strlen(s) - 1;
+	for (i = 0; i < strlen(s)/2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length];
+		s[length--] = temp;
+	}
+}
