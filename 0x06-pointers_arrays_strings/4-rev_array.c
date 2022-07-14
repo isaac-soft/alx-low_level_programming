6629d986aafd119a5146651a4b6cb0cda02a4faa
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-07-14 16:47:25 -0700

    Reverse string

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: parameter
+ * @n: length
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	j = n -1;
+	for (i = 0; i < j; i++)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+		j--;
+	}
+}
