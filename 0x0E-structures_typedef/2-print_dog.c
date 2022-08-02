70aad30e6585399af9e36bb8a8c52103568ac6fb
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-08-02 15:08:58 +0000

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,9 +11,9 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		printf("Dog profile is empty");
+		return;
 
 	printf("Name: %s\n", (d->name == NULL ? "(nil)" : d->name));
-	printf("Age: %f\n", (d->age == 0 ? 0 : d->age));
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", (d->owner == NULL ? "(nil)" : d->owner));
 }
