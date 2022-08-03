dce8b69057d8e9709753a8fd16bdd2e619dfb2b7
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-08-03 03:51:40 +0000

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-
+/**
+ * free_dog - frees memory allocated to dog
+ * @d: parameter
+ * Return: void
+ */
 void free_dog(dog_t *d)
 {
 	if (d)
