e537964c50f27ce95f66010395ee8216eef67eae
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-08-03 03:58:39 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,7 +46,7 @@ char *_copy(char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *snoopie;
+	dog_t *new_dog;
 	char *new_name, *new_owner;
 
 	if (name == NULL || owner == NULL)
@@ -54,8 +54,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	snoopie = malloc(sizeof(dog_t));
-	if (snoopie == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
@@ -63,21 +63,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_name = _copy(name);
 	if (new_name == NULL)
 	{
-		free(snoopie);
+		free(new_dog);
 		return (NULL);
 	}
-	(*snoopie).name = new_name;
+	(*new_dog).name = new_name;
 
-	(*snoopie).age = age;
+	(*new_dog).age = age;
 
 	new_owner = _copy(owner);
 	if (new_owner == NULL)
 	{
-		free((*snoopie).name);
-		free(snoopie);
+		free((*new_dog).name);
+		free(new_dog);
 		return (NULL);
 	}
-	(*snoopie).owner = new_owner;
+	(*new_dog).owner = new_owner;
 
-	return (snoopie);
+	return (new_dog);
 }
