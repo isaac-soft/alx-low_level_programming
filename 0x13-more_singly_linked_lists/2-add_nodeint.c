0ab982b1aa0c329f53ddc5357bb02925df789cbe
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-08-16 11:54:52 +0000

    prepend a linked list with a node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_nodeint -add node at the beginning of a linked list
+ * @head: points to the head of a node
+ * @n: pointer the node value
+ *
+ * Return: address of the new element, NULL (on failure)
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node; /* creates pointer for every node added */
+
+	if (head == NULL)
+		return (NULL);
+
+	/* creating memory for new node */
+	new_node = malloc(sizeof(listint_t));
+	/* check if memory has been created */
+	if (new_node == NULL)
+		return (NULL);
+
+	/*code to acess, add and arrange node */
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
---
+++ 0x13-more_singly_linked_lists/2-mmain.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    printf("%d\n", head->n);
+    return (0);
+}
