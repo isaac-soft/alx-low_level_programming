1895b192222325789d13c9441d40f4141370c0f1
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-08-16 21:52:20 +0000

    insert node at given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,25 +13,45 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int counter;
 	listint_t *temp_node, *new_node;
+	(void)temp_node; /*handling unused variable case */
 
-	/* copy pointer location of head */
-	temp_node = *head;
-	/* move head to index position */
-	for (counter = 0; counter < (idx - 1); counter++)
+	if (*head == NULL)
 	{
-		temp_node = temp_node->next;
-		if (temp_node == NULL)
-		{
-			free(temp_node);
-			return (NULL);
-		}
+		return (NULL);
+}
+	
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n; /* give new_node its data */
+
+	if (idx == 0)
+	{
+		new_node->next = *head;	/*copy head data to temporary */
+		*head = new_node; /* the new_node is prepended and now the head */
+		return (new_node);
 	}
 
-	/* allocate memory for new node */
-	new_node = malloc(sizeof(listint_t));
-	new_node->n = n; /* give new_node its data */
-	new_node->next = temp_node->next; /* linke new node to next node */
-	temp_node->next = new_node; /* link previously linked node to new node*/
-	/* free(temp_node);*/
-	return (new_node);
+	else
+	{
+		/*copy head data to temporary */
+		temp_node = *head;
+		/* move head to index position */
+		for (counter = 0; counter < (idx - 1); counter++)
+		{
+			temp_node = temp_node->next;
+			if (temp_node == NULL)
+			{
+				free(temp_node);
+				return (NULL);
+			}
+		}
+		new_node->next = temp_node->next; /* linke new node to next node */
+		temp_node->next = new_node; /* link previously linked node to new node*/
+		/* free(temp_node);*/
+		return (new_node);
+	}
 }
