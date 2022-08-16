94d570a3ffca764db04c39cb5522077c0a61fb87
Isaac Oluwagbemi <oluwagbemiisaac@gmail.com>
2022-08-16 13:55:30 +0000

    free memory and set head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -18,11 +18,11 @@ void free_listint2(listint_t **head)
 	{
 		while (*head != NULL)
 		{
-			temp_node = *head;
-			*head = (*head)->next;
-			free(temp_node);
+			temp_node = (*head)->next;
+			free(*head);
+			*head = temp_node;
 		}
-		free(*head);
+		free(temp_node);
 	}
 	*head = NULL;
 }
